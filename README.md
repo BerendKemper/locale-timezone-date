e28045aa5f86542a7b6b04e01b78dc10ca91617c
Berend Kemper <berend_kemper@hotmail.com>
2020-08-08 00:53:08 +0200

    v1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,84 @@
-# locale-timezone-date
-JavaScript locale timezone Date module
+# localeTimezoneDate
+JavaScript locale time zone Date module
+
+<h3>localeTimezoneDate.toISOString(date[,options][,callback])</h3>
+<ul>
+    <li><code>date</code> &lt;Date></li>
+    <li><code>options</code> &lt;Object&gt;</li>
+    <ul>
+        <li><code>ms</code> &lt;Boolean&gt; Default: <code>true</code></li>
+    </ul>
+    <li><code>callback</code> &lt;Function&gt;</li>
+    <ul>
+        <li><code>err</code> &lt;Error&gt;</li>
+        <li><code>isoStr</code> &lt;String&gt;</li>
+    </ul>
+</ul>
+<h3>localeTimezoneDate.toFalseISOString(date[,options][,callback])</h3>
+<ul>
+    <li><code>date</code> &lt;Date></li>
+    <li><code>options</code> &lt;Object&gt;</li>
+    <ul>
+        <li><code>ms</code> &lt;Boolean&gt; Default: <code>true</code></li>
+    </ul>
+    <li><code>callback</code> &lt;Function&gt;</li>
+    <ul>
+        <li><code>err</code> &lt;Error&gt;</li>
+        <li><code>isoStr</code> &lt;String&gt;</li>
+    </ul>
+</ul>
+
+<h3>yyyymmdd.toString(date[,callback])</h3>
+<ul>
+    <li><code>date</code> &lt;Date></li>
+    <li><code>callback</code> &lt;Function&gt;</li>
+    <ul>
+        <li><code>err</code> &lt;Error&gt;</li>
+        <li><code>dateStr</code> &lt;String&gt;</li>
+    </ul>
+</ul>
+
+<h3>yyyymmdd.toMs(date[,callback])</h3>
+<ul>
+    <li><code>date</code> &lt;Date></li>
+    <li><code>callback</code> &lt;Function&gt;</li>
+    <ul>
+        <li><code>err</code> &lt;Error&gt;</li>
+        <li><code>ms</code> &lt;Number&gt;</li>
+    </ul>
+</ul>
+
+<h3>Examples</h3>
+<pre>
+<code class="language-javascript">npm i locale-time-zone-date
+
+const { localeTimezoneDate, yyyymmdd } = require("locale-time-zone-date");
+
+const localeISOString = localeTimezoneDate.toISOString(new Date());
+// returns "2020-08-06T13:06:50.261+0200"
+const regulatISOString = new Date(localeISOString).toISOString();
+// returns "2020-08-06T11:06:50.261Z"
+new Date(regulatISOString).getTime() === new Date(localeISOString).getTime()
+// returns true
+
+// Notice that my timezone is UTC +2 hours
+let date;
+date = new Date("2020-08-06T00:00:00.000Z");
+console.log(yyyymmdd.toString(date)); // returns "2020-08-06"
+console.log(yyyymmdd.toMs(date));     // returns 1596672000000
+date = new Date("2020-08-06T22:00:00.000Z");
+console.log(yyyymmdd.toString(date)); // returns "2020-08-07"
+console.log(yyyymmdd.toMs(date));     // returns 1596758400000
+
+date = new Date("2020-08-06T00:00:00.000+0200");
+console.log(yyyymmdd.toString(date)); // returns "2020-08-06"
+console.log(yyyymmdd.toMs(date));     // returns 1596672000000
+date = new Date("2020-08-06T22:00:00.000+0200");
+console.log(yyyymmdd.toString(date)); // returns "2020-08-06"
+console.log(yyyymmdd.toMs(date));     // returns 1596672000000
+
+localeTimezoneDate.toISOString(new Date(), (err, isoStr) => {
+    console.log(isoStr); // returns "2020-08-06T13:06:50.261+0200"
+});
+</code>
+</pre>
---
+++ index.js
@@ -0,0 +1,76 @@
+"use strict";
+const pad2 = number => number < 10 ? "0" + number : number;
+const pad3 = number => number < 10 ? "00" + number : number < 100 ? "0" + number : number;
+const getUTCOffset = date => {
+    const hour = date.getTimezoneOffset() / -60;
+    return { hhmm: "+" + pad2(hour) + "00", hour };
+};
+const defaultOptions_toISOString = { ms: true };
+const defaultCallback = (err, data) => data || err;
+/**
+ * Locale time zone Date ISO String
+ * @param {Date} date 
+ * @param {{ Boolean }} options.ms (optional) default true
+ * @param {Function} callback (optional)
+ * @returns {String} "yyyy-mm-ddThh:mm:ss[.ms]+hhmm"
+ */
+const localeTimezoneDate_toISOString = (date, options = defaultOptions_toISOString, callback = defaultCallback) => {
+    if (typeof options === "function") {
+        callback = options;
+        options = defaultCallback;
+    }
+    if (date instanceof Date)
+        return callback(null, pad2(date.getFullYear()) + "-" + pad2(date.getMonth() + 1) + "-" + pad2(date.getDate()) + "T" + pad2(date.getHours()) + ":" + pad2(date.getMinutes()) + ":" + pad2(date.getSeconds()) + (options.ms === true ? "." + pad3(date.getMilliseconds()) : "") + getUTCOffset(date).hhmm);
+    else
+        return callback(new TypeError("date must be a Date"), null);
+};
+/**
+ * Locale time zone Date false ISO String
+ * @param {Date} date 
+ * @param {{Object}} options.ms (optional) default true
+ * @param {Function} callback (optional)
+ * @returns {String} "yyyy-mm-ddThh:mm:ss[.ms]Z"
+ */
+const localeTimezoneDate_toFalseISOString = (date, options = defaultOptions_toISOString, callback = defaultCallback) => {
+    if (typeof options === "function") {
+        callback = options;
+        options = defaultCallback;
+    }
+    if (date instanceof Date)
+        return callback(null, pad2(date.getFullYear()) + "-" + pad2(date.getMonth() + 1) + "-" + pad2(date.getDate()) + "T" + pad2(date.getHours()) + ":" + pad2(date.getMinutes()) + ":" + pad2(date.getSeconds()) + (options.ms === true ? "." + pad3(date.getMilliseconds()) : "") + "Z");
+    else
+        return callback(new TypeError("date must be a Date"), null);
+};
+const localeTimezoneDate = Object.freeze({
+    'toISOString': localeTimezoneDate_toISOString,
+    'toFalseISOString': localeTimezoneDate_toFalseISOString
+});
+/**
+ * Locale time zone Date String
+ * @param {Date} date
+ * @param {Function} callback (optional)
+ * @returns {String} "yyyy-mm-dd"
+ */
+const yyyymmdd_toString = (date, callback = defaultCallback) => {
+    if (date instanceof Date)
+        return callback(null, pad2(date.getFullYear()) + "-" + pad2(date.getMonth() + 1) + "-" + pad2(date.getDate()));
+    else
+        return callback(new TypeError("date must be a Date"), null);
+};
+/**
+ * Locale time zone start time of the date in milliseconds
+ * @param {Date} date 
+ * @param {Function} callback (optional)
+ * @returns {Number} Number of milliseconds that have elapsed since the Unix epoch
+ */
+const yyyymmdd_toMs = (date, callback = defaultCallback) => {
+    if (date instanceof Date)
+        return callback(null, date.setHours(getUTCOffset(date).hour, 0, 0, 0));
+    else
+        return callback(new TypeError("date must be a Date"), null);
+};
+const yyyymmdd = Object.freeze({
+    'toString': yyyymmdd_toString,
+    'toMs': yyyymmdd_toMs
+});
+module.exports = Object.freeze({ localeTimezoneDate, yyyymmdd });
